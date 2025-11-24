02ed26cde7ca70a12ed0e7d11fbc65bea841bc65
ambikaaswal <aswalambika@gmail.com>
2025-11-24 12:23:00 +0530

    documentation information added

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,16 @@
+//This app contans one backend(Node + express) with two react apps named frontend and backend
+// and a ml-service for stock and sentiment prediction.
+
+//User flow:
+// frontend-> on login backend call for jwt verification-> on verification-> to dashboard->
+// dashboard-> calls made to backend for functionalities including ml service
+//->on logging out-> back to frontend.
+
+//create an .env file for all dashboard, frontend and backend folders, where 
+//in backend's .env: you should have a db/atlas mongodb url, a frontend port url, a dashboard port url, a ml-service port url:
+//in frontend's .env: you should have a backend url, a dashboard url,
+//in dashboard's .env: you should have a backend url and the frontend url
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
